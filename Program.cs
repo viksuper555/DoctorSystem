6b3e4dffdb2bf154a209fde06d5e8cbaad07915b
Viktor Kolev <viksuper555@gmail.com>
2022-10-17 20:14:56 +0300

    Add project files.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.  Dobaven
+// Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
