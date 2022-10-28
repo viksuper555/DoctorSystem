29775ca040564cd048a233c2825845bd2ab58d3e
viktor436 <eeeevv54@gmail.com>
2022-10-28 22:29:01 +0300

    регистрация

---
+++ Data/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace DoctorSystem.Data
+{
+    public class AppUser : IdentityUser
+    {
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string gender {get; set;}
+
+        [DataType(DataType.Date)]
+        public string dateOfBirth {get; set;}
+    }
+}
