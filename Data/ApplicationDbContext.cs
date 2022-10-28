9815aed85644afbef08ec3414e4ab328d88e57b8
viktor436 <eeeevv54@gmail.com>
2022-10-28 22:29:01 +0300

    регистрация

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,11 +3,34 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DoctorSystem.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<AppUser>()
+                .Property(e => e.firstName)
+                .HasMaxLength(250);
+
+            modelBuilder.Entity<AppUser>()
+                .Property(e => e.lastName)
+                .HasMaxLength(250);
+
+            modelBuilder.Entity<AppUser>()
+                .Property(e => e.gender)
+                .HasMaxLength(250);
+
+            modelBuilder.Entity<AppUser>()
+                .Property(e => e.dateOfBirth)
+                .HasMaxLength(250);
+
         }
     }
 }
