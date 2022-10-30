ea660c41a7045853d868654182c561c59d44d28a
viktor436 <eeeevv54@gmail.com>
2022-10-30 15:58:03 +0200

    additional fields

---
+++ Models/DefaultUser.cs
@@ -0,0 +1,18 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace DoctorSystem.Models
+{
+    public class DefaultUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Gender { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
