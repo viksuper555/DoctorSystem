9023a27490e30cc41d23468c4cbbb36e567afa3a
viktor436 <eeeevv54@gmail.com>
2022-10-30 15:58:03 +0200

    additional fields

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using DoctorSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DoctorSystem.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    public class ApplicationDbContext : IdentityDbContext<DefaultUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
@@ -15,21 +16,20 @@ namespace DoctorSystem.Data
 
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<AppUser>()
-                .Property(e => e.firstName)
+            modelBuilder.Entity<DefaultUser>()
+                .Property(e => e.FirstName)
                 .HasMaxLength(250);
 
-            modelBuilder.Entity<AppUser>()
-                .Property(e => e.lastName)
+            modelBuilder.Entity<DefaultUser>()
+                .Property(e => e.LastName)
                 .HasMaxLength(250);
 
-            modelBuilder.Entity<AppUser>()
-                .Property(e => e.gender)
+            modelBuilder.Entity<DefaultUser>()
+                .Property(e => e.Gender)
                 .HasMaxLength(250);
 
-            modelBuilder.Entity<AppUser>()
-                .Property(e => e.dateOfBirth)
-                .HasMaxLength(250);
+            modelBuilder.Entity<DefaultUser>()
+                .Property(e => e.DateOfBirth);
 
         }
     }
