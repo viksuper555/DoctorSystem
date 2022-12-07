81082a7907da4ba92cb15e100883f4d177ef4e72
Viktor Kolev <viksuper555@gmail.com>
2022-12-07 19:07:36 +0200

    Added friends data models

--- Models/DefaultUser.cs
+++ Models/DefaultUser.cs
@@ -16,5 +16,16 @@ namespace DoctorSystem.Models
         public DateTime? DateOfBirth { get; set; }
 
         public string? DoctorUID { get; set; }
+        public ICollection<UserFriendship> FriendsOf { get; set; }
+        public ICollection<UserFriendship> Friends { get; set; }
+    }
+
+    public class UserFriendship
+    {
+        public string UserId { get; set; }
+        public DefaultUser User { get; set; }
+
+        public string UserFriendId { get; set; }
+        public DefaultUser UserFriend { get; set; }
     }
 }
