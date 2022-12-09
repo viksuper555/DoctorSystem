047161c7e00dea7be9b05ce321fc60c2f7c1efbe
viktor436 <eeeevv54@gmail.com>
2022-12-09 22:39:22 +0200

    Comments CRUD functionality added.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -14,7 +14,7 @@ namespace DoctorSystem.Data
         public DbSet<Post> Post { get; set; }
         public DbSet<Comment> Comment { get; set; }
         public DbSet<RoleRequest> RoleRequests { get; set; }
-
+       
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
