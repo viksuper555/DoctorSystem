af6a4a0272b394efab0170b003a80144962a43eb
Viktor Kolev <15073936+viksuper555@users.noreply.github.com>
2022-12-13 15:28:57 +0200

    Added friends data models (#16)

---
+++ Data/Migrations/20221207170404_Friends.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoctorSystem.Data.Migrations
+{
+    public partial class Friends : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Post",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatorId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Post", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Post_AspNetUsers_CreatorId",
+                        column: x => x.CreatorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoleRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RequesterId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RoleId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    IsApproved = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoleRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RoleRequests_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_RoleRequests_AspNetUsers_RequesterId",
+                        column: x => x.RequesterId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserFriendship",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UserFriendId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserFriendship", x => new { x.UserId, x.UserFriendId });
+                    table.ForeignKey(
+                        name: "FK_UserFriendship_AspNetUsers_UserFriendId",
+                        column: x => x.UserFriendId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UserFriendship_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PostId = table.Column<int>(type: "int", nullable: false),
+                    CreatorId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comment_AspNetUsers_CreatorId",
+                        column: x => x.CreatorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Comment_Post_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Post",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_CreatorId",
+                table: "Comment",
+                column: "CreatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_PostId",
+                table: "Comment",
+                column: "PostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Post_CreatorId",
+                table: "Post",
+                column: "CreatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoleRequests_RequesterId",
+                table: "RoleRequests",
+                column: "RequesterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoleRequests_RoleId",
+                table: "RoleRequests",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserFriendship_UserFriendId",
+                table: "UserFriendship",
+                column: "UserFriendId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserClaims_AspNetUsers_UserId",
+                table: "AspNetUserClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserLogins_AspNetUsers_UserId",
+                table: "AspNetUserLogins");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserRoles_AspNetUsers_UserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                name: "Comment");
+
+            migrationBuilder.DropTable(
+                name: "RoleRequests");
+
+            migrationBuilder.DropTable(
+                name: "UserFriendship");
+
+            migrationBuilder.DropTable(
+                name: "Post");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PostId",
+                table: "AspNetRoleClaims",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TempId",
+                table: "AspNetRoleClaims",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TempId1",
+                table: "AspNetRoleClaims",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TempId2",
+                table: "AspNetRoleClaims",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TempId3",
+                table: "AspNetRoleClaims",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Text",
+                table: "AspNetRoleClaims",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_AspNetRoleClaims_TempId",
+                table: "AspNetRoleClaims",
+                column: "TempId");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_AspNetRoleClaims_TempId1",
+                table: "AspNetRoleClaims",
+                column: "TempId1");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_AspNetRoleClaims_TempId2",
+                table: "AspNetRoleClaims",
+                column: "TempId2");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_AspNetRoleClaims_TempId3",
+                table: "AspNetRoleClaims",
+                column: "TempId3");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserClaims_AspNetRoleClaims_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId",
+                principalTable: "AspNetRoleClaims",
+                principalColumn: "TempId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserLogins_AspNetRoleClaims_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId",
+                principalTable: "AspNetRoleClaims",
+                principalColumn: "TempId1",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserRoles_AspNetRoleClaims_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId",
+                principalTable: "AspNetRoleClaims",
+                principalColumn: "TempId2",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserTokens_AspNetRoleClaims_UserId",
+                table: "AspNetUserTokens",
+                column: "UserId",
+                principalTable: "AspNetRoleClaims",
+                principalColumn: "TempId3",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
