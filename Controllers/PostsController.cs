772e6842d7c00bf57de3e7eed45a799be0c45937
viktor436 <eeeevv54@gmail.com>
2022-12-15 22:11:49 +0200

    MyPosts is working now.User can onlyvedit/delete his posts/comments.

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -42,6 +42,14 @@ namespace DoctorSystem.Controllers
             return View( _context.Post.Include(p => p.Comments).ThenInclude(x => x.Creator).Include(t => t.Creator).Include(v=>v.Category).ToList());
         }
 
+        public ViewResult IndexPatient(string searchString)
+        {
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                return View(_context.Post.Where(s => s.Creator.Email == User.Identity.Name).Where(s => s.Category.Name == searchString).Include(p => p.Comments).ThenInclude(x => x.Creator).Include(t => t.Creator).Include(v => v.Category).ToList());
+            }
+             return View(_context.Post.Where(s => s.Creator.Email == User.Identity.Name).Include(p => p.Comments).ThenInclude(x => x.Creator).Include(t => t.Creator).Include(v => v.Category).ToList());
+        }
         // GET: Posts/Details/5
         public async Task<IActionResult> Details(int? id)
         {
