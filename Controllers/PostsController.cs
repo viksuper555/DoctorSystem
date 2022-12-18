060802f29beff43a9b8df3c943c89f23f1e3290f
viktor436 <eeeevv54@gmail.com>
2022-12-18 16:10:08 +0200

    Bug patient access denied resolved. Code improvements.

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -33,35 +33,31 @@ namespace DoctorSystem.Controllers
             _userManager = userManager;
         }
 
-        [Authorize(Roles = "Doctor")]
         public ViewResult Index(string searchString)
         {
-            if (!String.IsNullOrEmpty(searchString))
+            var str = _context.Post
+                        .Include(p => p.Comments)
+                        .ThenInclude(x => x.Creator)
+                        .Include(t => t.Creator)
+                        .OrderByDescending(s => s.DateCreated)
+                        .Include(v => v.Category).ToList();
+            if (User.IsInRole(Role.Doctor))
             {
-                return View(_context.Post
-                    .Where(s => s.Category.Name == searchString)
-                    .Include(p => p.Comments)
-                    .ThenInclude(x => x.Creator)
-                    .Include(t => t.Creator)
-                    .OrderByDescending(s => s.DateCreated)
-                    .Include(v=>v.Category).ToList());
+                if (!String.IsNullOrEmpty(searchString))
+                {
+                    return View(str.Where(s => s.Category.Name == searchString));
+                }
+                return View(str);
             }
-            return View( _context.Post.Include(p => p.Comments).ThenInclude(x => x.Creator).Include(t => t.Creator).Include(v=>v.Category).OrderByDescending(s => s.DateCreated).ToList());
-        }
-
-        [Authorize(Roles = "Patient")]
-        public ViewResult IndexPatient(string searchString)
-        {
-            if (!String.IsNullOrEmpty(searchString))
+            else
             {
-                return View(_context.Post.Where(s => s.Creator.Email == User.Identity.Name)
-                    .Where(s => s.Category.Name == searchString)
-                    .Include(p => p.Comments).ThenInclude(x => x.Creator).Include(t => t.Creator)
-                    .Include(v => v.Category).OrderByDescending(s => s.DateCreated).ToList());
+                if (!String.IsNullOrEmpty(searchString))
+                {
+                    return View(str.Where(s => s.Creator.Email == User.Identity.Name)
+                        .Where(s => s.Category.Name == searchString));
+                }
+                return View(str.Where(s => s.Creator.Email == User.Identity.Name));
             }
-             return View(_context.Post.Where(s => s.Creator.Email == User.Identity.Name)
-                 .Include(p => p.Comments).ThenInclude(x => x.Creator).Include(t => t.Creator)
-                 .Include(v => v.Category).OrderByDescending(s => s.DateCreated).ToList());
         }
         // GET: Posts/Details/5
         public async Task<IActionResult> Details(int? id)
@@ -270,7 +266,7 @@ namespace DoctorSystem.Controllers
             }
             ViewData["PostId"] = new SelectList(_context.Post, "Id", "Description", comment.PostId);
             return View(comment);
-        }  
+        }
 
         public async Task<IActionResult> DeleteComment(int? id)
         {
